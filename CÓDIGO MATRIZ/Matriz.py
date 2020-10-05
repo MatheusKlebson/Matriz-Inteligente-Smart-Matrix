c3abdf1821fc7c0735e564668862a8a6c26fb6ca
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-05 20:17:41 -0300

    Alimentando matriz

--- CÓDIGO MATRIZ/Matriz.py
+++ CÓDIGO MATRIZ/Matriz.py
@@ -5,3 +5,7 @@ def cabeçalho(txt):
     print("="*tam)
 cabeçalho("               MATRIZ INTELIGENTE              ")
 matriz = [[0,0,0],[0,0,0],[0,0,0]]
+for linha in range(0,3):
+    for coluna in range(0,3):
+        matriz[linha][coluna] = int(input(f"Digite o valor da casa [{linha},{coluna}]: "))
+print(matriz)
