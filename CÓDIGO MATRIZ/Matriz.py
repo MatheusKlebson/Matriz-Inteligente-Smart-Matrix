1cb946c01b3a78e548fa3418e216d19ef06a8dc0
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-05 20:14:19 -0300

    Cabeçalho

---
+++ CÓDIGO MATRIZ/Matriz.py
@@ -0,0 +1,7 @@
+def cabeçalho(txt):
+    tam = len(txt) + 5
+    print("="*tam)
+    print(txt)
+    print("="*tam)
+cabeçalho("               MATRIZ INTELIGENTE              ")
+matriz = [[0,0,0],[0,0,0],[0,0,0]]
