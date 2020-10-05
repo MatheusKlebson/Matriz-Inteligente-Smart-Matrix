cba34445ad8e8ddc32c975e21431028021db904f
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-05 20:20:01 -0300

    Matriz formada

--- CÓDIGO MATRIZ/Matriz.py
+++ CÓDIGO MATRIZ/Matriz.py
@@ -8,4 +8,8 @@ matriz = [[0,0,0],[0,0,0],[0,0,0]]
 for linha in range(0,3):
     for coluna in range(0,3):
         matriz[linha][coluna] = int(input(f"Digite o valor da casa [{linha},{coluna}]: "))
-print(matriz)
+
+for l in range(0,3):
+    for c in range(0,3):
+        print(f"[{matriz[l][c]:^5}]",end=" ")
+    print()
