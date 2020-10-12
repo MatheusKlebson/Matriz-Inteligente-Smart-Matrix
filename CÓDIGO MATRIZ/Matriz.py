f204f021ca13d1508baed5735e1a911bab762846
MatheusKlebson <68394225+MatheusKlebson@users.noreply.github.com>
2020-10-12 12:10:49 -0300

    CÓDIGOS MATRIZ

--- CÓDIGO MATRIZ/Matriz.py
+++ CÓDIGO MATRIZ/Matriz.py
@@ -3,13 +3,24 @@ def cabeçalho(txt):
     print("="*tam)
     print(txt)
     print("="*tam)
+
+
 cabeçalho("               MATRIZ INTELIGENTE              ")
 matriz = [[0,0,0],[0,0,0],[0,0,0]]
 for linha in range(0,3):
     for coluna in range(0,3):
         matriz[linha][coluna] = int(input(f"Digite o valor da casa [{linha},{coluna}]: "))
-
+cabeçalho("                 MATRIZ FORMADA                ")
 for l in range(0,3):
     for c in range(0,3):
         print(f"[{matriz[l][c]:^5}]",end=" ")
     print()
+print("="*52)
+cabeçalho("                 MENU DE OPÇÕES                ")
+opção = int(input('''[1] - ANALISAR LINHA
+[2] - ANALISAR COLUNA
+[3] - ANALISAR NÚMERO PELA POSIÇÃO
+[4] - CALCULAR NÚMEROS ESPECIFICOS
+[5] - CRIAR OUTRA MATRIZ
+[6] - SAIR DO PROGRAMA
+Digite: '''))
